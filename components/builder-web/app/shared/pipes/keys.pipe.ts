b5b71bcca3f85cc2d4b62c2587aa47f1f3539f36
Yauhen Artsiukhou <jsirex@gmail.com>
2019-10-17 17:30:26 +0300

    Backport habitat/master at 67f408a70883cdebeb44287ac7a19085718aa286

--- components/builder-web/app/shared/pipes/keys.pipe.ts
+++ components/builder-web/app/shared/pipes/keys.pipe.ts
@@ -1,6 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({ name: 'habKeysPipe', pure: false })
+@Pipe({ name: 'bioKeysPipe', pure: false })
 export class KeysPipe implements PipeTransform {
   transform(value: any, args: any[] = null): any {
     return Object.keys(value);
