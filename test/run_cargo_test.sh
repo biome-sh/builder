0f504b399832c502bb358a264c6982eea11a1efb
Fork Man <forkman@biome.sh>
2021-10-16 07:23:24 +0000

    Backport habitat/main at 69422267c566b7f0fdfc04906ff76b220f301771

--- test/run_cargo_test.sh
+++ test/run_cargo_test.sh
@@ -17,6 +17,7 @@ sudo bio pkg install core/zeromq
 sudo bio pkg install core/pkg-config
 sudo bio pkg install core/protobuf
 sudo bio pkg install core/postgresql
+sudo bio pkg install core/cmake
 # It is important NOT to use a vendored openssl from openssl-sys
 # pg-sys does not use openssl-sys. So for components that use
 # diesel's postgres feature, you wil end up with 2 versions of openssl
@@ -26,7 +27,7 @@ export LD_RUN_PATH
 LD_RUN_PATH="$(bio pkg path core/glibc)/lib:$(bio pkg path core/gcc-libs)/lib:$(bio pkg path core/openssl)/lib:$(bio pkg path core/postgresql)/lib:$(bio pkg path core/zeromq)/lib:$(bio pkg path core/libarchive)/lib"
 export PKG_CONFIG_PATH
 PKG_CONFIG_PATH="$(bio pkg path core/zeromq)/lib/pkgconfig:$(bio pkg path core/libarchive)/lib/pkgconfig:$(bio pkg path core/postgresql)/lib/pkgconfig:$(bio pkg path core/openssl)/lib/pkgconfig"
-eval "$(bio pkg env core/rust/"$toolchain"):$(bio pkg path core/protobuf)/bin:$(bio pkg path core/pkg-config)/bin:$(bio pkg path core/postgresql)/bin:$PATH"
+eval "$(bio pkg env core/rust/"$toolchain"):$(bio pkg path core/protobuf)/bin:$(bio pkg path core/pkg-config)/bin:$(bio pkg path core/postgresql)/bin:$(bio pkg path core/cmake)/bin:$PATH"
 
 # Set testing filesystem root
 export TESTING_FS_ROOT
